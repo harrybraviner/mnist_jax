d961e86b9d79fbc87ddf3b124115aa2bfaa95560
Harry Braviner <harry.braviner@gmail.com>
2024-03-29 21:16:41 -0400

    CNN from tutorial

--- train.py
+++ train.py
@@ -1,26 +1,60 @@
 import jax
+import jax.numpy as jnp
+from jaxtyping import Float, Array
+import equinox as eqx
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torchvision
 
-from typing import Tuple
+from typing import Tuple, List
+
+
+class CNN(eqx.Module):
+    layers: List
+
+    def __init__(self, key):
+        # Architecture from Equinox tutorial. Single convolutional layer than 3-layer MLP. Parameterize later.
+        key1, key2, key3, key4 = jax.random.split(key, 4)
+        self.layers = [
+            eqx.nn.Conv2d(1, 3, kernel_size=4, key=key1),
+            eqx.nn.MaxPool2d(kernel_size=2),
+            jax.nn.relu,
+            jnp.ravel,
+            eqx.nn.Linear(1728, 512, key=key2),
+            jax.nn.sigmoid,  # Curious choice.
+            eqx.nn.Linear(512, 64, key=key3),
+            jax.nn.relu,
+            eqx.nn.Linear(64, 10, key=key4),
+            jax.nn.log_softmax,
+        ]
+
+    def __call__(self, x: Float[Array, "1 28 28"]) -> Float[Array, "10"]:
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
 
 def train(
         train_dataset: Dataset,
         validation_dataset: Dataset,
         # Random seeds
-        training_shuffle_seed: int,
+        seed: int,
         # Hyperparameters
         num_epochs: int,
         batch_size: int,
 ):
+    key_master = jax.random.PRNGKey(seed)
+    key_training_shuffle_seed, key_model_init = jax.random.split(key_master, 2)
+
     # Convert datasets into dataloaders
-    train_shuffle_generator = torch.Generator().manual_seed(training_shuffle_seed)
+    train_shuffle_generator = \
+        torch.Generator().manual_seed(jax.random.choice(key_training_shuffle_seed, 10000, shape=()).item())
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
                                                    num_workers=1, shuffle=True, generator=train_shuffle_generator)
     validation_dataloader = torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size,
                                                         num_workers=1, shuffle=False)
+
+    model = CNN(key_model_init)
 
 
 def main():
@@ -39,7 +73,7 @@ def main():
     train(
         train_dataset=train_dataset,
         validation_dataset=test_dataset,  # FIXME - switch to using a proper validation dataset
-        training_shuffle_seed=123,
+        seed=123,
         num_epochs=3,
         batch_size=64,
     )
